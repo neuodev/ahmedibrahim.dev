257ac448708c82b3804fbf64b7edd7746b754480
Ahmed Ghait <me@ahmedibrahim.dev>
2026-01-22 14:04:39 +0200

    feat: redirect traffic from ahmedibrahim.dev to ahmedghait.com

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  redirects: () => [
+    {
+      source: "/",
+      destination: "https://ahmedghait.com",
+      permanent: true,
+    },
+  ],
 };
 
 export default nextConfig;
